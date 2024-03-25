0b6daa3fd9bcd2a13a5be5804220cad88862f331
SergioRuiz <sergioruizrock@gmail.com>
2024-03-25 14:16:27 -0300

    se agrega el componente footer

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="text-center bg-dark py-2">
+            <p className="text-light ">&copy; Todos los derechos reservados</p>
+        </footer>
+    );
+};
+
+export default Footer;
