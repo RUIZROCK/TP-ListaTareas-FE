edaa5f8901dcd5a8898bf571798f27249c53ef2a
SergioRuiz <sergioruizrock@gmail.com>
2024-03-26 03:15:45 -0300

    se aplicaron y configuraron las rutas de cada componente para su funcionamiento

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,42 @@
-import './App.css'
+import "./App.css";
+import Cuerpo from "./components/pages/Cuerpo";
+import Footer from "./components/common/Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import FormTarea from "./components/pages/tarea/FormTarea";
+import VerTarea from "./components/pages/tarea/VerTarea";
+import Encabezado from "./components/common/Encabezado";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
+      <BrowserRouter>
+        <Encabezado></Encabezado>
+        <Container className="mainContainer">
+          <Routes>
+            <Route exact path="/" element={<Cuerpo></Cuerpo>}></Route>
+            <Route
+              exact
+              path="/detalleTarea/:id"
+              element={<VerTarea></VerTarea>}
+            ></Route>
+            <Route
+              exact
+              path="/crearTarea"
+              element={<FormTarea></FormTarea>}
+            ></Route>
+            <Route
+              exact
+              path="/editarTarea/:id"
+              element={<FormTarea editar={true}></FormTarea>}
+            ></Route>
+          </Routes>
+        </Container>
+        <Footer></Footer>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
